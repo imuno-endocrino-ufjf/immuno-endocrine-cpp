dcb6ab8828eba000054bf7e700f55f562565efd6
Samuel Nascimento <me@samnsc.com>
2024-12-12 22:45:56 -0300

    Rename SaveAllStates to IntegralObserver and log individual decade simulation duration

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -4,16 +4,24 @@
 #include <map>
 #include <vector>
 
+#if defined(ENABLE_PROFILING) && ENABLE_PROFILING == 1
+    #include <chrono>
+#endif
+
 namespace Utilities {
     std::map<double, std::vector<double>> combineStateWithTime(const std::vector<std::vector<double>> &states, const std::vector<double> &times);
 
-    class SaveAllStates {
+    class IntegralObserver {
         private:
             std::vector<std::vector<double>> &m_states;
             std::vector<double> &m_times;
 
+#if defined(ENABLE_PROFILING) && ENABLE_PROFILING == 1
+            std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<long long, std::ratio<1LL, 1000000000LL>>> previous_decade_time = std::chrono::high_resolution_clock::now();
+#endif
+
         public:
-            SaveAllStates(std::vector<std::vector<double>> &states, std::vector<double> &times);
+            IntegralObserver(std::vector<std::vector<double>> &states, std::vector<double> &times);
             void operator()(const std::vector<double> &x, double t);
     };
 }  // namespace Utilities
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,5 +1,13 @@
 #include "utilities.hpp"
 
+#if defined(ENABLE_PROFILING) && ENABLE_PROFILING == 1
+    #include <fmt/base.h>
+    #include <fmt/chrono.h>
+    #include <fmt/color.h>
+
+    #include <chrono>
+#endif
+
 namespace Utilities {
     std::map<double, std::vector<double>> combineStateWithTime(const std::vector<std::vector<double>> &states, const std::vector<double> &times) {
         if (states.size() != times.size()) {
@@ -15,9 +23,27 @@ namespace Utilities {
         return mapped_states;
     }
 
-    SaveAllStates::SaveAllStates(std::vector<std::vector<double>> &states, std::vector<double> &times): m_states(states), m_times(times) {}
+    IntegralObserver::IntegralObserver(std::vector<std::vector<double>> &states, std::vector<double> &times): m_states(states), m_times(times) {}
 
-    void SaveAllStates::operator()(const std::vector<double> &x, double t) {
+    void IntegralObserver::operator()(const std::vector<double> &x, double t) {
+#if defined(ENABLE_PROFILING) && ENABLE_PROFILING == 1
+        if (m_times.size() != 0) {
+            int previous_time = m_times[m_times.size() - 1];
+            int previous_time_decade = previous_time / 3650;
+
+            int current_time_decade = t / 3650;
+
+            if (previous_time_decade != current_time_decade) {
+                auto current_decade_time = std::chrono::high_resolution_clock::now();
+
+                auto current_decade_duration = std::chrono::duration_cast<std::chrono::microseconds>(current_decade_time - this->previous_decade_time);
+                fmt::print(fg(fmt::color::dark_golden_rod) | fmt::emphasis::bold, "->Finished decade {}. Decade simulation duration: {} ({})\n", current_time_decade, current_decade_duration, std::chrono::duration_cast<std::chrono::seconds>(current_decade_duration));
+
+                this->previous_decade_time = current_decade_time;
+            }
+        }
+#endif
+
         m_states.push_back(x);
         m_times.push_back(t);
     }
