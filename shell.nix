a2a979799c3522bdff5683ee22688e128d8af217
Samuel Nascimento <me@samnsc.com>
2025-01-28 23:47:29 -0300

    Add gnuplot overlay

--- shell.nix
+++ shell.nix
@@ -1,4 +1,17 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (final: prev: {
+      gnuplot = prev.gnuplot.overrideAttrs (old: {
+        version = "6.0.0";
+
+        src = prev.fetchurl {
+          url = "mirror://sourceforge/gnuplot/gnuplot-6.0.0.tar.gz";
+          sha256 = "sha256-Y1oo8Jk/arDRF54HKtObgTnQf1Ejf4Qdk8bC/0sXWOw=";
+        };
+      });
+    })
+  ];
+} }:
 
 pkgs.mkShell {
   packages = with pkgs; [
